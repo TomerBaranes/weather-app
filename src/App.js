08ca91f60c4ef51292c516041a64430e343eabd7
Tomer <tomerbaranes1@gmail.com>
2022-02-27 18:32:31 +0200

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import WeatherForecast from "./components/WeatherForecast";
+
+import "./App.css";
+
+const api = {
+  key: process.env.REACT_APP_API_KEY,
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+const unit = "metric";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weatherData, setWeatherData] = useState();
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    fetch(`${api.base}forecast?q=${query}&units=${unit}&APPID=${api.key}`)
+      .then((response) => {
+        if (!response.ok) throw new Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        setWeatherData(data);
+        setQuery("");
+      })
+      .catch((err) => alert(err.message));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={handleSubmit} className="form-bar">
+          <input
+            type="text"
+            placeholder="Look up a city"
+            onChange={handleChange}
+            value={query}
+            className="form-input"
+          />
+          <button type="submit" className="form-button">
+            Search
+          </button>
+        </form>
+
+        {weatherData && (
+          <div>
+            <h1>
+              {weatherData.city.name}, {weatherData.city.country}
+            </h1>
+            <div className="forecast">
+              <WeatherForecast data={weatherData} num="0" units={unit} />
+              <WeatherForecast data={weatherData} num="8" units={unit} />
+              <WeatherForecast data={weatherData} num="16" units={unit} />
+              <WeatherForecast data={weatherData} num="24" units={unit} />
+              <WeatherForecast data={weatherData} num="30" units={unit} />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import dateBuilder from "../functions/DateBuilder";
+
+import "./WeatherForecast.css";
+
+const WeatherForecast = ({ data, num, units }) => {
+  if (data) {
+    const { day, date, month, year } = dateBuilder(
+      new Date(data.list[num].dt_txt)
+    );
+    return (
+      <div className="card">
+        <img
+          className="card-image"
+          src={`http://openweathermap.org/img/wn/${data.list[num].weather[0].icon}@2x.png`}
+          alt="weather-despription"
+        />
+        <div className="title-degrees">
+          <p>
+            {data.list[num].main.temp}
+            {units === "metric" ? "°C" : "°"}
+          </p>
+          <hr className="seperator" />
+          <div className="header-description">Date</div>
+          <div className="description">
+            {day}, {date}, {month}, {year}
+          </div>
+          <div className="header-description">General</div>
+          <div className="description">
+            {data.list[num].weather[0].description}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default WeatherForecast;
