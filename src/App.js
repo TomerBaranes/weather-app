568b0665fcde381feead3fbcd957a9205f0167f9
Tomer <tomerbaranes1@gmail.com>
2022-02-28 13:38:28 +0200

    added responsiveness, stlying and error hadnling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import WeatherForecast from "./components/WeatherForecast";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 import "./App.css";
+import Swal from "sweetalert2";
 
 const api = {
   key: process.env.REACT_APP_API_KEY,
@@ -14,6 +16,8 @@ const unit = "metric";
 function App() {
   const [query, setQuery] = useState("");
   const [weatherData, setWeatherData] = useState();
+  const [error, setError] = useState("Look up a city/country");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e) => {
     setQuery(e.target.value);
@@ -22,16 +26,25 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    if (query === "") {
+      Swal.fire("Please enter something");
+      return;
+    }
+
+    setIsLoading(true);
+
     fetch(`${api.base}forecast?q=${query}&units=${unit}&APPID=${api.key}`)
       .then((response) => {
+        if (!response.ok) setIsLoading(false);
         if (!response.ok) throw new Error(response.statusText);
         return response.json();
       })
       .then((data) => {
         setWeatherData(data);
+        setIsLoading(false);
         setQuery("");
       })
-      .catch((err) => alert(err.message));
+      .catch((err) => setError(err.message + " :("), setWeatherData());
   };
 
   return (
@@ -40,7 +53,6 @@ function App() {
         <form onSubmit={handleSubmit} className="form-bar">
           <input
             type="text"
-            placeholder="Look up a city"
             onChange={handleChange}
             value={query}
             className="form-input"
@@ -50,18 +62,26 @@ function App() {
           </button>
         </form>
 
-        {weatherData && (
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
           <div>
-            <h1>
-              {weatherData.city.name}, {weatherData.city.country}
-            </h1>
-            <div className="forecast">
-              <WeatherForecast data={weatherData} num="0" units={unit} />
-              <WeatherForecast data={weatherData} num="8" units={unit} />
-              <WeatherForecast data={weatherData} num="16" units={unit} />
-              <WeatherForecast data={weatherData} num="24" units={unit} />
-              <WeatherForecast data={weatherData} num="30" units={unit} />
-            </div>
+            {weatherData ? (
+              <div>
+                <h1>
+                  {weatherData.city.name}, {weatherData.city.country}
+                </h1>
+                <div className="forecast">
+                  <WeatherForecast data={weatherData} num="0" units={unit} />
+                  <WeatherForecast data={weatherData} num="8" units={unit} />
+                  <WeatherForecast data={weatherData} num="16" units={unit} />
+                  <WeatherForecast data={weatherData} num="24" units={unit} />
+                  <WeatherForecast data={weatherData} num="30" units={unit} />
+                </div>
+              </div>
+            ) : (
+              <p className="filler-text">{error}</p>
+            )}
           </div>
         )}
       </div>
